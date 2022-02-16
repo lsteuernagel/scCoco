f6e53465b25083d94d883e3de49d95dbe71283b5
lsteuernagel <lsteuernagel@blade10.sf.mpg.de>
2022-02-16 16:39:12 +0100

    added function requirements and retrieval script

---
+++ R/expression_functions.R
@@ -0,0 +1,6 @@
+## need function to retiev expression for set of genes
+
+  # check whether id exists
+  # retreive data
+  # resolve conflicts with multiple ids
+  # llow to load from cached data
---
+++ R/general_functions.R
@@ -0,0 +1,17 @@
+## need median rank function
+
+
+## need region annoation function
+  # allow to specify certain node in graph
+  # include only top n median of voxels of substructures
+
+## need function to reduce matrix to specific annotation level
+  # graph handling
+  # take target level
+
+
+## need function that returns likely and other clusters as data.frame
+
+## need wrapper functio(s)
+  # execute above pipeline for sets of marker genes and store (intermediate) results
+  # need to allow confidence measure based on
